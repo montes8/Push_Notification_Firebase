c669447be329a7339858e31675a8c972cf233cd9
montes8 <emontesinos812@gmail.com>
2019-01-04 17:20:32 -0500

    configurando firebase

---
+++ app/src/main/java/com/example/tayler_gabbi/cloud_messaging/service/MyFirebaseInstanceService.java
@@ -0,0 +1,6 @@
+package com.example.tayler_gabbi.cloud_messaging.service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceService extends FirebaseInstanceIdService {
+}
